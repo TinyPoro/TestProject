5d83a69a17954ea14315886b4b726aa32efd41a2
tinyporo <ngophuongtuan@gmail.com>
2018-11-22 21:42:47 +0700

    Update comment blog

--- app/Console/Commands/CommentBlog.php
+++ app/Console/Commands/CommentBlog.php
@@ -24,13 +24,13 @@ class CommentBlog extends Command
     protected $description = 'Command description';
 
     private $client;
-//    private $client_id = 'app-toppick';
-    private $client_id = '1';
-//    private $client_secret = '4409970dbafd2e0d53a4eb0ecb9bd3e84559a05c';
-    private $client_secret = '8a7c32a3b8de072fa276e1a7adf0260bdb6e90e8';
-//
-//    private $host = 'https://phpfoxdev.toppick.vn';
-    private $host = 'http://phpfox.net';
+    private $client_id = 'app-toppick';
+//    private $client_id = '1';
+    private $client_secret = '4409970dbafd2e0d53a4eb0ecb9bd3e84559a05c';
+//    private $client_secret = '8a7c32a3b8de072fa276e1a7adf0260bdb6e90e8';
+
+    private $host = 'https://phpfoxdev.toppick.vn';
+//    private $host = 'http://phpfox.net';
 
     /**
      * Create a new command instance.
@@ -44,11 +44,12 @@ class CommentBlog extends Command
         $this->client = new Client();
     }
 
-    private $total_comments = 10000000;
-    private $comment_per_user = 2;
+    private $total_comments = 150000;
+    private $commented = 0;
+    private $comment_per_user = 6000;
 
-    private $min_blog_id = 1;
-    private $max_blog_id = 4;
+    private $min_blog_id = 31;
+    private $max_blog_id = 10851;
 
     private $comment_path = 'comments.txt';
     private $comments = [];
@@ -79,15 +80,17 @@ class CommentBlog extends Command
             $access_token = $this->getAccessToken($user);
             if(!$access_token) continue;
 
-            $comment_blog_ids = $this->getUniqueRandomNumbersWithinRange($this->min_blog_id, $this->max_blog_id, $this->comment_per_user);
+            $comment_blog_ids = $this->getUniqueRandomNumbersWithinRange($this->min_blog_id, $this->max_blog_id);
 
             foreach ($comment_blog_ids as $comment_blog_id){
                 $commented = $this->commentBlog($access_token, $comment_blog_id);
 
                 if($commented){
-                    $this->total_comments--;
+                    $this->commented++;
 
-                    if($this->total_comments === 0) return;
+                    if( $this->commented % 10000 === 0) dump("Đã comment ".$this->commented);
+
+                    if($this->total_comments === $this->commented) return;
                 }
             }
         }
@@ -110,9 +113,12 @@ class CommentBlog extends Command
         return rand($min, $max);
     }
 
-    public function getUniqueRandomNumbersWithinRange($min, $max, $quantity) {
+    public function getUniqueRandomNumbersWithinRange($min, $max) {
         $numbers = range($min, $max);
         shuffle($numbers);
+
+        $quantity = $this->getRandom(200, $this->comment_per_user);
+
         return array_slice($numbers, 0, $quantity);
     }
 
